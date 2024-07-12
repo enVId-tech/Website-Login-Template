8f980b015a90664a68d549b0ff74f38ccc3dad2e
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-12 15:37:44 -0700

    refactor: tried working on fetch
    Tried fetching

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,17 +1,16 @@
 // import RootLayout from '@/pages/layout.tsx';
-
-type HomeProps = {
-    staticData: string;
-};
-
-async function getData(): Promise<string> {
-    const res = await fetch('/api/realtime');
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch data');
+import dotenv from 'dotenv';
+import handler from '../api/realtime/page';
+dotenv.config({ path: '@/app/api/environment.local.env' });
+async function getData(): Promise<string | null> {
+    try {
+        const response = await fetch('http://localhost:3000/api/realtime');
+        const data = await response.json();
+        return data.timestamp;
+    } catch (error) {
+        console.error('Failed to fetch real-time data:', error);
+        return null;
     }
-
-    return res.json();
 }
 
 const Home = async () => {
