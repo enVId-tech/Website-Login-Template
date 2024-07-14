e2f08be88429d70793b888b68eb6e36c69c259e3
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-14 15:59:50 -0700

    feat(app): added homepage
    Added a Next.js optimized homepage

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -2,25 +2,65 @@ import RootLayout from "@/app/_components/layout.tsx";
 import getUserData from "@/app/api/getUserData.ts";
 import { UserData } from "@/app/api/interfaces.ts";
 import { redirect } from 'next/navigation';
+import styles from '@/assets/styles/home.module.scss';
 
-const Home = async () => {
-    // const data: any = await getData();
-    const data: UserData[] | null | undefined = await getUserData();
+interface HomeData {
+    error: string;
+    displayName: string;
+}
 
-    if (data === null || !data) {
-        redirect('/login');
+async function userData(): Promise<HomeData | undefined> {
+    try {
+        const userData: UserData[] | null | undefined = await getUserData();
+
+        if (userData === null) {
+            window.location.href = '/login';
+            return;
+        }
+
+        if (userData === undefined) {
+            console.error('Guest account');
+            setData({
+                firstName: 'Guest',
+                lastName: 'Account',
+                email: '',
+                profilePicture: '',
+                displayName: 'Guest Account',
+                hd: '',
+            })
+            return;
+        }
+
+        setData(userData![0]);
+    } catch (error: unknown) {
+        console.error('Error:', error as string);
+    }
+}
+
+export default async function Home(): Promise<React.JSX.Element> {
+    const data: HomeData | undefined = await userData();
+
+    if (!data) {
+        return <RootLayout>
+            <section id="home">
+                <div id="container">
+                    <h1>Welcome, Guest Account!</h1>
+                </div>
+            </section>
+        </RootLayout>
     }
 
     return (
         <RootLayout>
             <section id="home">
-                <div>
-                    <h1>Home Page</h1>
-                    <p>Static Data: {}</p>
+                <div id="container">
+                    <h1>Welcome, {data?.displayName}!</h1>
                 </div>
             </section>
         </RootLayout>
-    );
-};
+    )
+}
 
-export default Home;
+function setData(arg0: UserData) {
+    throw new Error("Function not implemented.");
+}
