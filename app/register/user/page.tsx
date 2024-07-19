959b457f52ea72420d8fa26d493c7cfdc9412f2e
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-18 21:30:56 -0700

    feat: added some scss for page

--- app/register/user/page.tsx
+++ app/register/user/page.tsx
@@ -1,21 +1,27 @@
 import styles from '@/styles/register-user.module.scss';
+import { Work_Sans } from 'next/font/google';
 import React from 'react';
+
+const Work_Sans500 = Work_Sans({
+    weight: "500",
+    subsets: ["latin"],
+})
 
 export default function RegisterUserPage(): React.JSX.Element {
     return (
         <div className={styles.registerUserPage}>
             <div className={styles.container}>
-                <h1>Register User</h1>
+                <h1 className={Work_Sans500.className}>Register User</h1>
                 <form>
-                    <label htmlFor="username">Username</label>
+                    <label htmlFor="username" className={Work_Sans500.className}>Username</label>
                     <input type="text" id="username" name="username" required />
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor="email" className={Work_Sans500.className}>Email</label>
                     <input type="email" id="email" name="email" required />
-                    <label htmlFor="password">Password</label>
+                    <label htmlFor="password" className={Work_Sans500.className}>Password</label>
                     <input type="password" id="password" name="password" required />
-                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <label htmlFor="confirmPassword" className={Work_Sans500.className}>Confirm Password</label>
                     <input type="password" id="confirmPassword" name="confirmPassword" required />
-                    <button>Register</button>
+                    <button className={Work_Sans500.className}>Register</button>
                 </form>
             </div>
         </div>
