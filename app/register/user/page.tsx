e4658fd1fabfe60d03ff6ca4dbfee5dd34a6bc3f
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-18 23:21:40 -0700

    feat(app): added new functions for client

--- app/register/user/page.tsx
+++ app/register/user/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from '@/styles/register-user.module.scss';
 import { Work_Sans } from 'next/font/google';
 import React from 'react';
@@ -7,12 +9,62 @@ const Work_Sans500 = Work_Sans({
     subsets: ["latin"],
 })
 
+interface RegisterUserProps {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+function registerUser({ username, email, password, confirmPassword }: RegisterUserProps): void {
+    "use server";
+    if (password !== confirmPassword) {
+        alert('Passwords do not match');
+        return;
+    }
+
+    fetch('http://localhost:3000/api/user/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: username,
+            email: email,
+            password: password,
+        }),
+    }).then((response: Response) => {
+        if (response.status === 200) {
+            alert('User registered successfully');
+        } else {
+            alert('User registration failed');
+        }
+    }).catch((error: unknown) => {
+        console.error('Error:', error);
+    });
+}
+
 export default function RegisterUserPage(): React.JSX.Element {
+    const usernameRef = React.useRef<HTMLInputElement>(null);
+    const emailRef = React.useRef<HTMLInputElement>(null);
+    const passwordRef = React.useRef<HTMLInputElement>(null);
+    const confirmPasswordRef = React.useRef<HTMLInputElement>(null);
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        registerUser({
+            username: usernameRef.current?.value as string,
+            email: emailRef.current?.value as string,
+            password: passwordRef.current?.value as string,
+            confirmPassword: confirmPasswordRef.current?.value as string,
+        });
+    }
+
     return (
         <div className={styles.registerUserPage}>
             <div className={styles.container}>
                 <h1 className={Work_Sans500.className}>Register User</h1>
-                <form>
+                <form className={styles.form} onSubmit={handleSubmit}>
                     <label htmlFor="username" className={Work_Sans500.className}>Username</label>
                     <input type="text" id="username" name="username" required />
                     <label htmlFor="email" className={Work_Sans500.className}>Email</label>
